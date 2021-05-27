843c0fc87a0adfe602248d250683df17ae83a0f0
ADBin <32976949+ADBin@users.noreply.github.com>
2021-05-27 21:03:39 +0800

    Update tampermonkey.js

--- tampermonkey.js
+++ tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         移除互动弹幕
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       ADBIN
 // @match        *://*.bilibili.com/video/*
@@ -12,9 +12,10 @@
 
 (function() {
     'use strict';
-    console.log("JqueryVersion: ",$.fn.jquery)  //显示jquery版本
+    console.log("JqueryVersion: ",$.fn.jquery);  //显示jquery版本
     //console.log("aaa")
     $("#bilibiliPlayer").bind("DOMNodeInserted",function(e){
+        $(".bilibili-player-link-wrap").children().remove();
         $(".bilibili-player-popup-area").children().remove();
     })
 })();
